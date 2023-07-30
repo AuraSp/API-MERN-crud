b1ac47137f6d001fc7fe50d2362d0186ec982605
Aura <ArSpej@gmail.com>
2023-07-31 02:49:36 +0300

    Proxy URL added

--- server/app.js
+++ server/app.js
@@ -7,9 +7,17 @@ const teachersRoutes = require("./routes/teachersRoutes");
 const app = express();
 
 app.use(cors({
-origin: [""]
+origin: [process.env.PROXY_URI]
 }));
+
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`
+    <h2>API is working and silently waiting to be called</h2>
+    <h4>Check objects in json format:</h4>
+    <a href="${process.env.PROXY_URI}api/v2/students">Students Archive Object</a> <a href="${process.env.PROXY_URI}api/v2/teachers">Teachers Archive Object</a>`);
+})
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
